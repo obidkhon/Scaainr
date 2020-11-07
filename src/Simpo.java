ef5ba09376c300b4eb914ab9452c808a3c76499c
obidkon <obid.sharipov.80@mail.ru>
2020-11-07 20:18:22 +0500

    Scanner

---
+++ src/Simpo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Simpo {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("birinchi foydalanuvchini 1:");
+        String birinchi1=scan.nextLine();
+
+        System.out.println("birinchi foydalanuvchini 2:");
+        String birinchi2=scan.nextLine();
+
+        System.out.println("birinchi foydalanuvchini 3:");
+        String birinchi3=scan.nextLine();
+
+        System.out.println("birinchi foydalanuvchini 4:");
+        String birinchi4=scan.nextLine();
+
+
+        System.out.println("birinchi foydalanuvchini 5:");
+        String birinchi5=scan.nextLine();
+
+        System.out.println("birinchi foydalanuvchini 6:");
+        String birinchi6=scan.nextLine();
+
+
+        System.out.println("Mana birinchi");
+        System.out.println(birinchi1);
+        System.out.println(birinchi2);
+        System.out.println(birinchi3);
+        System.out.println(birinchi4);
+        System.out.println(birinchi5);
+        System.out.println(birinchi6);
+
+
+
+
+
+    }
+}
